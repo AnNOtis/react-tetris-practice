be1dd3c4916f43039dab419ab60754281140ea62
Otis <otis@hdtech.com.tw>
2014-12-03 00:59:53 +0800

    add meet stack and game over conditions

--- app/js/cell.jsx
+++ app/js/cell.jsx
@@ -4,11 +4,17 @@ module.exports = React.createClass({
   displayName: 'Cell',
   render: function(){
     var color = this.props.color
-    var style = {};
+    var style = {
+      boxSizing: "border-box",
+      width: "20px",
+      height: "20px",
+      border: "1px solid #999"
+    };
+    style["width"]  = "20px";
+    style["heigth"] = "20px";
     if( color !== 0){
-      style["background-color"] = color;
-      // style["border"] = "2px solid #333";
-      style["box-sizing"] = "border-box";
+      style["backgroundColor"] = color;
+      style["boxSizing"] = "border-box";
     }
     return (
       <div className="cell" style={style}>
--- app/js/gameboard.jsx
+++ app/js/gameboard.jsx
@@ -94,19 +94,12 @@ module.exports = React.createClass({
     }
   },
   componentWillMount: function(){
-    this.setState({
-      currentBlock: this.getNextBlock(),
-      nextBlock: this.getNextBlock()
-    });
+    this.updateCurrentBlock();
   },
   componentDidMount: function(){
-    setInterval(function() {
-      var currentBlock = this.state.currentBlock;
-      currentBlock.position[1]++;
-      this.setState({
-        currentBlock: currentBlock
-      })
-    }.bind(this), 50000);
+    this.state.dropInterval = setInterval(function() {
+      this.drop()
+    }.bind(this), 500);
     key('up, down, left, right', this.handleKeyDown);
   },
   generate2DArray: function(xSize, ySize){
@@ -119,10 +112,30 @@ module.exports = React.createClass({
     };
     return grid;
   },
-  getNextBlock: function(){
+  randomBlock: function(){
     var blocks = ["I","J","L","O","S","T","Z"];
     var choose = Math.floor(Math.random()*(blocks.length-1));
-    return this.props[blocks[choose]];
+    var resultBlock = this.props[blocks[choose]];
+    var cloneBlock = {
+      name: resultBlock.name,
+      shape: _.map(resultBlock.shape, _.clone),
+      position: _.map(resultBlock.position, _.clone),
+      color: resultBlock.color
+    }
+    return cloneBlock;
+  },
+  updateCurrentBlock: function(){
+    if(this.state.nextBlock === null){
+      this.setState({
+        currentBlock: this.randomBlock(),
+        nextBlock: this.randomBlock()
+      });
+    }else{
+      this.setState({
+        currentBlock: this.state.nextBlock,
+        nextBlock: this.randomBlock()
+      });
+    }
   },
   updateBlockPosition: function(){
 
@@ -137,10 +150,11 @@ module.exports = React.createClass({
   },
   renderGrid: function(){
   },
-  generateGridData: function () {
+  generateGridData: function (isStack) {
     var block = this.state.currentBlock;
     var range = this.updateRectangle(block);
-    var gridData = _.map( this.state.gridData, _.clone);
+    var gridData = isStack ? this.state.gridData : _.map( this.state.gridData, _.clone);
+
     var shapeIndexX = 0;
     for (var x = range.x1; x <= range.x2; x++) {
       var shapeIndexY = 0;
@@ -178,14 +192,29 @@ module.exports = React.createClass({
     var block = this.state.currentBlock;
     var xPosition = block.position[0] + x;
     var yPosition = block.position[1] + y;
-    console.log(block.shape);
-    if(this.meetEdge(xPosition, yPosition, block.shape)){
+    if(this.meetEdge(xPosition, yPosition, block.shape) || this.meetStack( block.shape, xPosition, yPosition)){
       return false;
     }else{
       block.position[0] = xPosition;
       block.position[1] = yPosition;
       this.forceUpdate();
     }
+  },
+  drop: function(){
+    var currentBlock = this.state.currentBlock;
+    bottomEdge = currentBlock.position[1] + currentBlock.shape.length-1
+    if( bottomEdge+1 > this.props.ySize-1
+      || this.meetStack(currentBlock.shape, currentBlock.position[0], currentBlock.position[1]+1)){
+      this.state.gridDate = this.generateGridData(true);
+      this.updateCurrentBlock();
+      if (currentBlock.position[1] == this.props[currentBlock.name].position[1]){
+        clearInterval(this.state.dropInterval);
+        alert("Game Over!!");
+      }
+    }else{
+      currentBlock.position[1]++;
+    }
+    this.forceUpdate();
   },
   meetEdge: function(xPosition, yPosition, shape){
     var leftEdge, rightEdge, bottomEdge;
@@ -197,21 +226,30 @@ module.exports = React.createClass({
         if(shape[y][x]===1 && x>=rightEdge-xPosition || rightEdge===undefined){
           rightEdge = xPosition + x;
         }
-        if(shape[y][x]===1 && y>=bottomEdge || bottomEdge===undefined){
+        if(shape[y][x]===1 && y>=bottomEdge-yPosition || bottomEdge===undefined){
           bottomEdge = yPosition + y;
         }
       }
     }
-    console.log(xPosition);
-    console.log(leftEdge+","+rightEdge+","+bottomEdge);
+    // console.log(leftEdge+","+rightEdge+","+bottomEdge);
     if(leftEdge<0 || rightEdge>this.props.xSize-1 || bottomEdge>this.props.ySize-1){
       return true;
     }else{
       return false;
     }
   },
-  meetStack: function(shape, x, y){
-
+  meetStack: function(shape, xPosition, yPosition){
+    for(var y = 0; y <= shape.length-1; y++){
+      for(var x = 0; x<= shape[0].length-1; x++){
+        if(this.state.gridData[x+xPosition][y+yPosition]!==0 && shape[y][x]){
+          console.log(x+xPosition);
+          console.log(y+yPosition);
+          console.log(shape);
+          return true;
+        }
+      }
+    }
+    return false;
   },
   rotateCurrentBlock: function(){
     var block = this.state.currentBlock;
